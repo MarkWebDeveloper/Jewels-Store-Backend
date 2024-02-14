8dd2cd3d9942b9ecd5c5caaa0059ee7f58a08468
Mark Kil <markkilcanarias@gmail.com>
2024-02-14 20:24:09 +0100

    first commit

---
+++ src/main/java/dev/mark/jewelsstorebackend/JewelsStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.mark.jewelsstorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JewelsStoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JewelsStoreBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mark/jewelsstorebackend/JewelsStoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mark.jewelsstorebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JewelsStoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
