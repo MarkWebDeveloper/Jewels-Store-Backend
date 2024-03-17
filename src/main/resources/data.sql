9566823eed72c414d5a0423d771a3ad873132230
Mark Kil <markkilcanarias@gmail.com>
2024-03-17 11:22:08 +0100

    esmeralda description changed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,7 +12,7 @@ INSERT INTO categories (id_category, category_name) VALUES (default, 'Ring & Ear
 /*Category 1*/
 INSERT INTO products (id_product, product_name, product_description, price) VALUES (default, 'Flamenco Abanico Earrings', 'Passionate design inspired by flamenco dancers fans', 19.99);
 INSERT INTO products (id_product, product_name, product_description, price) VALUES (default, 'Scheherazade Blue Earrings', 'Magical earrings for your evening dress', 29.99);
-INSERT INTO products (id_product, product_name, product_description, price) VALUES (default, 'Esmeralda Earrings', 'Este es, sin duda, el marcapáginas perfecto para leer la novela «Tiburón», de Peter Benchley.', 29.99);
+INSERT INTO products (id_product, product_name, product_description, price) VALUES (default, 'Esmeralda Earrings', 'Beatiful green earrings Esmeralda', 29.99);
 
 
 /*Categories-Products*/
