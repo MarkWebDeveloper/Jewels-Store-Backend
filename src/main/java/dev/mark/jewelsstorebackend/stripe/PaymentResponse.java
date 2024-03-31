a8b7bb33eec983e61b01375f943f9a70b185754b
Mark Kil <markkilcanarias@gmail.com>
2024-03-31 16:40:13 +0200

    added getter and setter to payment response

--- src/main/java/dev/mark/jewelsstorebackend/stripe/PaymentResponse.java
+++ src/main/java/dev/mark/jewelsstorebackend/stripe/PaymentResponse.java
@@ -3,11 +3,15 @@ package dev.mark.jewelsstorebackend.stripe;
 import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Setter
+@Getter
 @Builder
 public class PaymentResponse {
     private String clientSecret;
