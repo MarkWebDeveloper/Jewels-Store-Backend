864bde755182b5467689ba27534a67edb99d1989
Mark Kil <144998902+MarkWebDeveloper@users.noreply.github.com>
2024-05-21 13:05:51 +0200

    all configuration moved to securityconfiguration

--- src/main/java/dev/mark/jewelsstorebackend/config/SecurityConfiguration.java
+++ src/main/java/dev/mark/jewelsstorebackend/config/SecurityConfiguration.java
@@ -2,27 +2,64 @@ package dev.mark.jewelsstorebackend.config;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
+import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+
+import dev.mark.jewelsstorebackend.config.oauth.JWTtoUserConverter;
+import dev.mark.jewelsstorebackend.config.oauth.KeyUtils;
+
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @EnableJpaAuditing
 public class SecurityConfiguration {
     @Value("${api-endpoint}")
     String endpoint;
+
+    @Autowired
+    JWTtoUserConverter jwtToUserConverter; 
+
+    @Autowired
+    KeyUtils keyUtils; 
+
+    @Autowired
+    PasswordEncoder passwordEncoder; 
+    
+    @Autowired
+    UserDetailsManager userDetailsManager; 
 
     // JpaUserDetailService jpaUserDetailService;
 
@@ -38,28 +75,76 @@ public class SecurityConfiguration {
         http
                 .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
-                .formLogin(form -> form.disable());
                 // .logout(out -> out
                 //         .logoutUrl(endpoint + "/logout")
                 //         .deleteCookies("JSSESIONID"))
-                // .authorizeHttpRequests(auth -> auth
-                //         .requestMatchers(HttpMethod.GET, endpoint + "/login").hasAnyRole("ADMIN","USER")
-                //         .requestMatchers(HttpMethod.GET, endpoint + "/users").hasRole("ADMIN")
-                //         .requestMatchers(HttpMethod.PUT, endpoint + "/users/eventSignUp/{id}").hasAnyRole("ADMIN","USER")
-                //         .requestMatchers(HttpMethod.GET, endpoint + "/cities").permitAll()
-                //         .requestMatchers(HttpMethod.GET, endpoint + "/events").permitAll()
-                //         .requestMatchers(HttpMethod.POST, endpoint + "/events").hasRole("ADMIN")
-                //         .requestMatchers(HttpMethod.POST, endpoint + "/images").permitAll()
-                //         .requestMatchers(HttpMethod.POST, endpoint + "/users").permitAll()
-                //         .requestMatchers(HttpMethod.GET, endpoint + "/images/**").permitAll()
-                //         .anyRequest().authenticated())
-                // .userDetailsService(jpaUserDetailService)
-                // .httpBasic(Customizer.withDefaults())
-                // .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED));
+                .authorizeHttpRequests((authorize) -> authorize 
+                                .requestMatchers("/api/auth/*").permitAll() 
+                                .anyRequest().authenticated() 
+                ) 
+                .httpBasic(Customizer.withDefaults())
+                .oauth2ResourceServer((oauth2) -> 
+                        oauth2.jwt((jwt) -> jwt.jwtAuthenticationConverter(jwtToUserConverter)) 
+                )
+                .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) 
+                .exceptionHandling((exceptions) -> exceptions 
+                                .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint()) 
+                                .accessDeniedHandler(new BearerTokenAccessDeniedHandler()) 
+                );
         
         http.headers(header -> header.frameOptions(frame -> frame.sameOrigin()));
 
         return http.build();
+    }
+
+    @Bean
+    @Primary
+    JwtDecoder jwtAccessTokenDecoder() { 
+        return NimbusJwtDecoder.withPublicKey(keyUtils.getAccessTokenPublicKey()).build(); 
+    } 
+  
+    @Bean
+    @Primary
+    JwtEncoder jwtAccessTokenEncoder() { 
+        JWK jwk = new RSAKey 
+                .Builder(keyUtils.getAccessTokenPublicKey()) 
+                .privateKey(keyUtils.getAccessTokenPrivateKey()) 
+                .build(); 
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk)); 
+        return new NimbusJwtEncoder(jwks); 
+    } 
+  
+    @Bean
+    @Qualifier("jwtRefreshTokenDecoder") 
+    JwtDecoder jwtRefreshTokenDecoder() { 
+        return NimbusJwtDecoder.withPublicKey(keyUtils.getRefreshTokenPublicKey()).build(); 
+    } 
+  
+    @Bean
+    @Qualifier("jwtRefreshTokenEncoder") 
+    JwtEncoder jwtRefreshTokenEncoder() { 
+        JWK jwk = new RSAKey 
+                .Builder(keyUtils.getRefreshTokenPublicKey()) 
+                .privateKey(keyUtils.getRefreshTokenPrivateKey()) 
+                .build(); 
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk)); 
+        return new NimbusJwtEncoder(jwks); 
+    } 
+  
+    @Bean
+    @Qualifier("jwtRefreshTokenAuthProvider") 
+    JwtAuthenticationProvider jwtRefreshTokenAuthProvider() { 
+        JwtAuthenticationProvider provider = new JwtAuthenticationProvider(jwtRefreshTokenDecoder()); 
+        provider.setJwtAuthenticationConverter(jwtToUserConverter); 
+        return provider; 
+    } 
+  
+    @Bean
+    DaoAuthenticationProvider daoAuthenticationProvider() { 
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(); 
+        provider.setPasswordEncoder(passwordEncoder); 
+        provider.setUserDetailsService(userDetailsManager); 
+        return provider; 
     }
 
     @Bean
