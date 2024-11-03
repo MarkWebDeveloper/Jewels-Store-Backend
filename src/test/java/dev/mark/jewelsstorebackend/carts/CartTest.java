0bc801b77f64dc18a818f17ac60822851bdb35a9
Mark Kil <144998902+MarkWebDeveloper@users.noreply.github.com>
2024-11-03 10:52:40 +0100

    cart model test is added

---
+++ src/test/java/dev/mark/jewelsstorebackend/carts/CartTest.java
@@ -0,0 +1,34 @@
+package dev.mark.jewelsstorebackend.carts;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import dev.mark.jewelsstorebackend.cart.Cart;
+import dev.mark.jewelsstorebackend.cart.item.CartItem;
+import dev.mark.jewelsstorebackend.products.Product;
+
+public class CartTest {
+
+    private Cart cart;
+    private Product product1;
+
+    {
+        cart = Cart.builder().id(1L).build();
+        product1 = Product.builder().id(1L).productName("Product1").build();
+        CartItem cartItem1 = new CartItem(1L, product1, 1L, cart);
+        List<CartItem> cartItems = new ArrayList<>();
+        cartItems.add(cartItem1);
+        cart.setCartItems(cartItems);
+    }
+
+    @Test
+    void test_cart_has_id_and_cartItems() {
+        assertThat(cart.getId(), is(1L));
+        assertThat(cart.getCartItems().get(0).getProduct().getProductName(), is("Product1"));
+    }
+}
